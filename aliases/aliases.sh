69e07497dbed63458f73e8212fa4be4e26bf8015
Matthew Nitschke <matthew.nitschke@workiva.com>
2018-07-16 10:29:43 -0600

    modifications to configurations

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -1,3 +1,10 @@
-alias python="python3"
+SCRIPT_DIR=$(dirname "$0") # get the base directory for this file
 
+alias python="python3"
 alias copy="clipcopy"
+
+if [[ $OSTYPE == darwin* ]]; then
+    source $SCRIPT_DIR/osx.sh
+else
+    source $SCRIPT_DIR/linux.sh
+fi
---
+++ aliases/osx.sh
@@ -0,0 +1,5 @@
+# spotify aliases (uses the oh-my-zsh spotify app thing contained in the osx package)
+alias sp="spotify"
+alias sp:math="spotify play uri spotify:user:1242045734:playlist:5iRg1hUvSGW6jP8DW7duPy"
+alias sp:rap="spotify play uri spotify:user:1242045734:playlist:17KeQcvfwQWgF8jomrkO7Q"
+alias sp:alt="spotify play uri spotify:user:1242045734:playlist:0UHS0SucJrcymLmqzCzTJd"
