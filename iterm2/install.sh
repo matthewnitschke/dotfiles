f61340d8a8e46ec0900c7f1998cc885b5c748fe1
Matthew <matthewcnitschke@gmail.com>
2018-09-03 20:45:02 -0600

    added fixes to make init command

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$0") # get the base directory for this file
+
 # iterm2 is osx only, ensure we are on an osx machine
 if [[ $OSTYPE == darwin* ]]; then
     if [ -d "$HOME/Library/Application Support/iTerm2/DynamicProfiles" ]; then
         # only setup iterm2 config if iterm2 is installed
-        ln -f "./iterm2-profile.plist" "$HOME/Library/Application Support/iTerm2/DynamicProfiles/iterm2-profile.plist"
+        ln -f "$SCRIPT_DIR/iterm2-profile.plist" "$HOME/Library/Application Support/iTerm2/DynamicProfiles/iterm2-profile.plist"
     else
         echo "iterm2 not installed, skipping iterm2 config setup"
     fi
