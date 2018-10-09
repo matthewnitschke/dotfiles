343cb62500d385905ee2c05458945b01a4df941f
Matthew Nitschke <matthewcnitschke@gmail.com>
2018-10-09 12:23:15 -0600

    added git open plugin

--- zsh/update-plugins.sh
+++ zsh/update-plugins.sh
@@ -2,6 +2,8 @@ SCRIPT_DIR=$(dirname "$0") # get the base directory for this file
 
 rm -rf $SCRIPT_DIR/plugins/zsh-autosuggestions
 rm -rf $SCRIPT_DIR/plugins/zsh-syntax-highlighting
+rm -rf $SCRIPT_DIR/plugins/git-open
 
 git clone https://github.com/zsh-users/zsh-autosuggestions $SCRIPT_DIR/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $SCRIPT_DIR/plugins/zsh-syntax-highlighting
+git clone https://github.com/paulirish/git-open.git $SCRIPT_DIR/plugins/git-open
