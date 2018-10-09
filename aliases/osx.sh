6a5f3a1e29d2d5254338eece22c3449c23050a27
Matthew Nitschke <matthewcnitschke@gmail.com>
2018-10-09 12:58:49 -0600

    updated rap playlist spotify url

--- aliases/osx.sh
+++ aliases/osx.sh
@@ -1,7 +1,7 @@
 # spotify aliases (uses the oh-my-zsh spotify app thing contained in the osx package)
 alias sp="spotify"
 alias sp:math="spotify play uri spotify:user:1242045734:playlist:5iRg1hUvSGW6jP8DW7duPy"
-alias sp:rap="spotify play uri spotify:user:1242045734:playlist:17KeQcvfwQWgF8jomrkO7Q"
+alias sp:rap="spotify play uri spotify:user:1242045734:playlist:3XMO2yBkB9YHVRM23gMGyL"
 alias sp:alt="spotify play uri spotify:user:1242045734:playlist:0UHS0SucJrcymLmqzCzTJd"
 alias sp:tech="spotify play uri spotify:user:1242045734:playlist:2XjuySZCdqT6PnBchVgwd8"
 alias sp:jazz="spotify play uri spotify:user:1242045734:playlist:7N2ToGpwgpx5bGnu8xCvgE"
