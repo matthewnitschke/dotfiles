dca122cb90f98ebaa40dec112a2cd912efa10cba
Matthew <matthewcnitschke@gmail.com>
2018-10-13 11:44:07 -0600

    added aliases

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -10,6 +10,9 @@ alias gr='cd $(git rev-parse --show-toplevel)'
 
 alias checkoff="node ~/scripts/checkoff/checkoff.js"
 
+alias gh="cd ~/Programming/GitHub"
+alias gl="cd ~/Programming/GitLab"
+
 if [[ $OSTYPE == darwin* ]]; then
     source $SCRIPT_DIR/osx.sh
 else
