3d416929bea05a19d5140d9fe5a2643158ccb3f6
Matthew <matthewcnitschke@gmail.com>
2018-11-02 10:37:19 -0600

    added homebrew installation

--- brew/install.sh
+++ brew/install.sh
@@ -3,13 +3,15 @@ SCRIPT_DIR=$(dirname "$0") # get the base directory for this file
 if [[ $OSTYPE == darwin* ]]; then
     # If we are running on osx
 
-    if [ -x "$(command -v brew)" ]; then
-        pushd $SCRIPT_DIR # make sure you are in the correct directory for installing brew dependencies
-        brew bundle # if the brew command is actually installed, install dependencies from the bundle
-        popd
-    else
-        echo "Brew not installed, not installing brew bundle"
+    if test ! $(which brew); then
+        echo "Installing homebrew..."
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
+
+    pushd $SCRIPT_DIR # make sure you are in the correct directory for installing brew dependencies
+    brew bundle # install dependencies from the bundle
+    popd
+
 else
     echo "Not darwin system, not installing brew bundle"
 fi
