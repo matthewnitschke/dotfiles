a6a7243822fbd573234f079e4c4904c2b550eb35
Matthew <matthewcnitschke@gmail.com>
2018-12-08 19:32:56 -0700

    fixed dependencies

--- system-dependencies/install.sh
+++ system-dependencies/install.sh
@@ -3,19 +3,25 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
         echo "Installing homebrew..."
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
+
     brew install git
     brew install zsh
     brew install node
+
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     if [[ -x "$(command -v pacman)" ]]; then
+
         pacman -Sy git
         pacman -Sy zsh
         pacman -Sy nodejs npm
+
     elif [[ -x "$(command -v apt-get)" ]]; then
+
         apt-get --yes install git
         apt-get --yes install zsh
 
         curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
         apt-get --yes install nodejs
+        
     fi
 fi
