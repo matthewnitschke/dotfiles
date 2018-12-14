5bddd4d298fddf256030966e6c0d0c96704cb7c7
Matthew Nitschke <matthewcnitschke@gmail.com>
2018-12-14 16:03:38 -0700

    decided I didnt like pet in this

--- pet/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(dirname "$0") # get the base directory for this file
-
-if [[ -x "$(command -v pet)" ]]; then
-    ln -f "$SCRIPT_DIR/config.toml" "$HOME/.config/pet/config.toml"
-else
-    echo "pet not installed, please install with desired package manager first"
-fi
