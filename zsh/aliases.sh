a7f84774e6e31608f4d7043091ac0024eb268bab
Matthew Nitschke <matthewcnitschke@gmail.com>
2020-06-15 20:53:13 -0600

    added code alias

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -5,10 +5,7 @@ alias sudo='sudo ' # fixes the issue of running aliases with sudo
 alias desk="cd ~/Desktop"
 alias mic="micro"
 alias lg="lazygit"
-alias p="pet exec"
-
-alias gh="cd ~/Programming/GitHub"
-alias gl="cd ~/Programming/GitLab"
+alias c="code ./"
 
 sb() {
   local branches branch
@@ -16,24 +13,3 @@ sb() {
   branch=$(echo "$branches" | fzf +m -i) &&
   git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
 }
-
-sbr() {
-  local branches branch
-  branches=$(git branch --all | grep -v HEAD) &&
-  branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m -i) &&
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
-
-typora() {
-	open $1 -a 'Typora'
-}
-
-# spotify aliases (uses the oh-my-zsh spotify app thing contained in the osx package)
-alias sp="spotify"
-alias sp:math="spotify play uri spotify:user:1242045734:playlist:5iRg1hUvSGW6jP8DW7duPy"
-alias sp:rap="spotify play uri spotify:user:1242045734:playlist:3XMO2yBkB9YHVRM23gMGyL"
-alias sp:alt="spotify play uri spotify:user:1242045734:playlist:0UHS0SucJrcymLmqzCzTJd"
-alias sp:tech="spotify play uri spotify:user:1242045734:playlist:2XjuySZCdqT6PnBchVgwd8"
-alias sp:jazz="spotify play uri spotify:user:1242045734:playlist:7N2ToGpwgpx5bGnu8xCvgE"
-
