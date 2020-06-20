0c5642c7dd9f40687f1f689a76bd68696b2cc25f
Matthew Nitschke <matthewcnitschke@gmail.comi>
2020-06-20 14:21:27 -0600

    removed no longer used deps

--- vim/install.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -f ~/.vim/bundle/Vundle.vim ]; then
-    # if vundle is not installed, install it
-    echo "Installing Vundle"
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
-
-pushd ~
-vim -c 'PluginInstall' -c 'qa!'
-popd
