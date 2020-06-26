9964f7a69b14cde3d9a56011a5f3e48c9023b5c1
Matthew Nitschke <matthewcnitschke@gmail.com>
2020-06-25 22:37:49 -0600

    added check

--- terminator/install.sh
+++ terminator/install.sh
@@ -7,4 +7,6 @@ if [ -x "$(command -v terminator)" ]; then
     mkdir -p ~/.config/terminator # ensure config folder exists
     
     ln -f $SCRIPT_DIR/config ~/.config/terminator/config
+else
+    printf "Terminator not installed, please install then re-run the command\n"
 fi
