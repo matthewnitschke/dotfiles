2918933caf0c5b59f505eb51daca90f108946f44
Matthew Nitschke <matthewcnitschke@gmail.com>
2020-06-25 22:51:12 -0600

    fixed install script for zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,22 +4,24 @@ function __join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/
 
 SCRIPT_DIR=$(dirname "$0") # get the base directory for this file
 
-SHOULD_COPY_TEMPLATE=$(cat ~/.zshrc | grep -q "# Installed by matthewnitschke/dotfiles/zsh/install.sh")
+# uninstall all plugins
+rm -rf $SCRIPT_DIR/plugins
+
+# install real plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions $SCRIPT_DIR/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $SCRIPT_DIR/plugins/zsh-syntax-highlighting
+git clone https://github.com/paulirish/git-open.git $SCRIPT_DIR/plugins/git-open
+
+# install fake plugins (these are ones that already are installed within oh-my-zsh)
+printf "Dummy file so that ~/dotfiles/zsh/install.sh thinks the filename is a plugin" > $SCRIPT_DIR/plugins/git
+
+SHOULD_COPY_TEMPLATE=$(cat $HOME/.zshrc | grep "# Installed by matthewnitschke/dotfiles/zsh/install.sh")
 if [ ! -n "$SHOULD_COPY_TEMPLATE" ]; then
     printf "Installing ~/.zshrc template\n"
     PLUGIN_LIST=$(ls ./plugins)
     PLUGINS=$(__join_by '\ \n    ' $PLUGIN_LIST)
 
-    cat zshrc.template.sh | sed "s/{{plugins}}/git\ \n    $PLUGINS/g" > ~/.zshrc
+    cat zshrc.template.sh | sed "s/{{plugins}}/$PLUGINS/g" > ~/.zshrc
 else 
-    printf "Template already installed, not overriding"
+    printf "Template already installed, not overriding\n"
 fi
-
-# install oh-my-zsh plugins
-rm -rf $SCRIPT_DIR/plugins/zsh-autosuggestions
-rm -rf $SCRIPT_DIR/plugins/zsh-syntax-highlighting
-rm -rf $SCRIPT_DIR/plugins/git-open
-
-git clone https://github.com/zsh-users/zsh-autosuggestions $SCRIPT_DIR/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $SCRIPT_DIR/plugins/zsh-syntax-highlighting
-git clone https://github.com/paulirish/git-open.git $SCRIPT_DIR/plugins/git-open
