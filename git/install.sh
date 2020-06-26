e54cb7eab967842953cf69a59bf38d11e971bebc
Matthew Nitschke <matthewcnitschke@gmail.com>
2020-06-25 22:29:14 -0600

    fixes and additions

--- git/install.sh
+++ git/install.sh
@@ -1,3 +1,5 @@
+GITCONFIG_PATH="$HOME/.gitconfig"
+
 # Verify we arent already installed
 if grep -q "include" "$GITCONFIG_PATH"; then
     echo "[include] already exists within ~/.gitconfig, unable to add script"
@@ -8,8 +10,6 @@ pushd . > /dev/null
 cd $(dirname "$0")
 CONFIG_SCRIPT_PATH=$(pwd)/gitconfig
 popd > /dev/null
-
-GITCONFIG_PATH="$HOME/.gitconfig"
 
 # Add the [include] tag to the users gitconifg
 printf "\n[include]\n\tpath = $CONFIG_SCRIPT_PATH\n" >> "$GITCONFIG_PATH"
