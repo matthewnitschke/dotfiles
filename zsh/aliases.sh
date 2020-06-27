0ff5ac533f476bb335ee5fe236fe2c5b38120e68
Matthew Nitschke <matthewcnitschke@gmail.com>
2020-06-26 21:33:31 -0600

    fixes

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -3,7 +3,7 @@ alias sudo='sudo ' # fixes the issue of running aliases with sudo
 alias desk="cd ~/Desktop"
 alias mic="micro"
 alias lg="lazygit"
-alias c="code ./"
+alias c="code --extensions-dir $HOME/dotfiles/extensions --user-data-dir $HOME/dotfiles/user_data"
 
 sb() {
   local branches branch
