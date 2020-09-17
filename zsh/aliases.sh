1f593a5a443449ef7b941daecfd385ead776c2d1
Matthew Nitschke <matthew.nitschke@workiva.com>
2020-09-17 11:17:02 -0600

    fixes

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,4 +1,4 @@
-alias sudo='sudo ' # fixes the issue of running aliases with sudo
+# alias sudo='sudo ' # fixes the issue of running aliases with sudo
 
 alias desk="cd ~/Desktop"
 alias mic="micro"
@@ -13,23 +13,6 @@ sb() {
   git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
 }
 
-sbr() {
-  local branches branch
-  branches=$(git branch --all | grep -v HEAD) &&
-  branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m -i) &&
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
-
-typora() {
-	open $1 -a 'Typora'
-}
-
 stripcolor() {
   sed $'s,\x1b\\[[0-9;]*[a-zA-Z],,g'
-}
-
-gcam() {
-  git add ./
-  git commit -m $1
 }
