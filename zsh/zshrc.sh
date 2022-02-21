05ec593437a46b796f98b5e32e09f2b54058bcda
Matthew Nitschke <matthew.nitschke@workiva.com>
2022-02-21 09:02:47 -0700

    added setopt commands for zsh history

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -14,4 +14,13 @@ if [[ -x "$(command -v yarn)" ]]; then
     export PATH="$(yarn global bin):$PATH"
 fi
 
+# Zsh History Config
+setopt append_history     # Allow multiple terminal sessions to all append to one zsh command history
+setopt inc_append_history # Add comamnds as they are typed, don't wait until shell exit
+setopt hist_expire_dups_first # when trimming history, lose oldest duplicates first
+setopt hist_ignore_dups   # Do not write events to history that are duplicates of previous events
+setopt hist_ignore_space  # remove command line from history list when first character on the line is a space
+setopt hist_find_no_dups  # When searching history don't display results already cycled through twice
+setopt hist_reduce_blanks # Remove extra blanks from each command line being added to history
+
 export EDITOR="micro"
